821ba9e264d666a4a6efc4237f2b8cb12023a7e6
Ginachiko1998 <soniagina669@gmail.com>
2024-06-11 23:30:35 +0700

    make change

--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -10,14 +10,6 @@ const NavbarComponent = () => {
           <FaHeart className="me-2" /> {/* Tambahkan ikon di sini */}
           Wedding of Lian & Gina
         </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" style={{ color: 'white' }}>Home</Nav.Link>
-            <Nav.Link href="#about" style={{ color: 'white' }}>About</Nav.Link>
-            <Nav.Link href="#contact" style={{ color: 'white' }}>Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
